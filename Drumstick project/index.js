1682a6214779403d0349f48f66f0a94020b69e30
Dev <devvarshney11@gmail.com>
2023-09-05 19:37:40 +0530

    Completing the drumskit project

--- Drumstick project/index.js
+++ Drumstick project/index.js
@@ -1,6 +1,15 @@
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function (event) {
     const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
-    if (!audio) return;
+    if (!audio)
+        return;
     audio.currentTime = 0;
     audio.play();
-})
+    const box = document.querySelector(`div[data-key="${event.keyCode}"]`);
+    if (!box)
+        return;
+    box.classList.toggle('highlight');
+    box.addEventListener('transitionend',function()
+    {
+        box.classList.remove('highlight');
+    })
+});
